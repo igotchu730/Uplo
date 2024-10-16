9e88da2d2f0a140d3a8f79df7e327ab202384d8a
igotchu730 <145312829+igotchu730@users.noreply.github.com>
2024-10-15 22:10:25 -0700

    Frontend Fixes & Staging Box
    Restructure and fix some HTML and CSS. Added in staging box after user selects files.

--- mainpage/index.js
+++ mainpage/index.js
@@ -1,8 +1,21 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const uploadBox = document.querySelector("#upload");
-    const fileInput = document.querySelector("#uploadFile");
+const uploadBox = document.getElementById('upload'); // The whole upload box.
+const fileInput = document.getElementById('uploadFile'); // The file the user uploads.
+const stagingBox = document.getElementById('staging'); // The staging box after users choose files.
 
-    uploadBox.addEventListener("click", function() {
+// When the page is loaded...
+document.addEventListener("DOMContentLoaded", function() {
+    uploadBox.addEventListener("click", function() { // The upload box when clicked will open the file explorer.
         fileInput.click();
     });
+    stagingBox.style.display = 'none'; // Hide the staging box
 });
+
+// After the user selects file(s), the upload box will disappear and the staging box will appear.
+fileInput.addEventListener('change',(event) => {
+    const files = event.target.files;
+
+    if(files.length > 0){
+        uploadBox.style.display = 'none';
+        stagingBox.style.display = 'flex';
+    }
+})
