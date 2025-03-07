7cdc79b2873970b989768742d8ce2e3c5812fde7
igotchu730 <145312829+igotchu730@users.noreply.github.com>
2025-03-06 21:10:52 -0800

    UTC Fix
    update so datetime is handled on mysql side

--- utility.js
+++ utility.js
@@ -53,20 +53,15 @@ async function insertFileUpload(id, ipAddress, fileName, pageLink, s3Link, fileS
       // upload size
       const uploadSize = fileSize;
 
-      // Time fields 
-      // Get current time in UTC, and adjust format for mysql
-      //const currentTime = DateTime.now().setZone('America/Los_Angeles').toSQL({ includeOffset: false });
-      // Expiration time (set to 24 hours later), and adjust format for mysql
-      //const expirationTime = DateTime.now().setZone('America/Los_Angeles').plus({ hours: 24 }).toSQL({ includeOffset: false });
-      
-      // Get current time in UTC, formatted for MySQL
-      const currentTime = DateTime.utc().toSQL({ includeOffset: false });
-      // Expiration time (24 hours later), formatted for MySQL
-      const expirationTime = DateTime.utc().plus({ hours: 24 }).toSQL({ includeOffset: false });
 
       // Insert the hashed data into the file_uploads table
-      const query = 'INSERT INTO file_uploads (id, ip_address, ip_hash, file_name, page_link, s3_link, file_size, expiration_date, created_at) VALUES (?, ?, SHA2(?, 256), ?, ?, ?, ?, ?, ?)';
-      const values = [id, encryptedIpAddress,ipAddress, encryptedFileName, encryptedPageLink, encryptedS3Link, uploadSize, expirationTime, currentTime];
+      const query = `
+          INSERT INTO file_uploads 
+          (id, ip_address, ip_hash, file_name, page_link, s3_link, file_size, expiration_date, created_at) 
+          VALUES (?, ?, SHA2(?, 256), ?, ?, ?, ?, DATE_ADD(UTC_TIMESTAMP(), INTERVAL 24 HOUR), UTC_TIMESTAMP());
+      `;
+      
+      const values = [id, encryptedIpAddress, ipAddress, encryptedFileName, encryptedPageLink, encryptedS3Link, fileSize];
 
       pool.query(query, values, (err, results) => {
           if (err) {
